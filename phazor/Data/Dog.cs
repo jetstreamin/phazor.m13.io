aca82c3be29d55216ba034029759d817f8a4cbcd
Michael Mahon <mmahon512@gmail.com>
2019-07-29 01:37:51 -0500

    added fetch button

--- phazor/Data/Dog.cs
+++ phazor/Data/Dog.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace phazor.Data {
     public class Dog {
         [JsonProperty("message")]
-        public string ImageUrl { get; set; } = "https://httpstatusdogs.com/img/404.jpg";
+        public string ImageUrl { get; set; } = "https://ui-ex.com/images/transparent-blue-loading-image-gif-3.gif";
         public string status { get; set; }        
     }
 }
