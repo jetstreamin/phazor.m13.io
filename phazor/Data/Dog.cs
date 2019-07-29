ee5ef1245686aca50b05b0090e6a8e8e6718061e
Michael Mahon <mmahon512@gmail.com>
2019-07-29 01:25:24 -0500

    init commit

---
+++ phazor/Data/Dog.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phazor.Data {
+    public class Dog {
+        [JsonProperty("message")]
+        public string ImageUrl { get; set; } = "https://httpstatusdogs.com/img/404.jpg";
+        public string status { get; set; }        
+    }
+}
---
+++ phazor/Data/DogService.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace phazor.Data
+{
+    public class DogService {
+        public async Task<Dog> GetRandomDogImageAsync() {
+            Dog dog = new Dog();
+            var uri = "https://dog.ceo/api/breeds/image/random";
+            using (var client = new HttpClient()) {
+                var json = await client.GetStringAsync(uri);
+                dog = JsonConvert.DeserializeObject<Dog>(json);
+            }
+            return dog;
+        }
+    }
+}
