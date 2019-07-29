446306f201439fdd2779cd9a1b1eb819a1194b23
Michael Mahon <mmahon512@gmail.com>
2019-07-29 03:00:35 -0500

    Trying a serverless approach...  still no-go

--- phazor/Function.cs
+++ phazor/Function.cs
@@ -1,32 +1,32 @@
-using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.Json;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading.Tasks;
+//using Amazon.Lambda.Core;
+//using Amazon.Lambda.RuntimeSupport;
+//using Amazon.Lambda.Serialization.Json;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Hosting;
+//using System;
+//using System.Threading.Tasks;
 
 
-namespace phazor
-{
-    public class Function
-    {
-        public static async Task Main(string[] args)
-        {
-            Func<string, ILambdaContext, string[]> func = FunctionHandler;
-            using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer()))
-            using (var bootstrap = new LambdaBootstrap(handlerWrapper))
-            {
-                await bootstrap.RunAsync();
-            }
-        }
+//namespace phazor
+//{
+//    public class Function
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//            Func<string, ILambdaContext, string[]> func = FunctionHandler;
+//            using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer()))
+//            using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+//            {
+//                await bootstrap.RunAsync();
+//            }
+//        }
 
-        public static IHostBuilder FunctionHandler(string input, ILambdaContext context, string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+//        public static IHostBuilder FunctionHandler(string input, ILambdaContext context, string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//            .ConfigureWebHostDefaults(webBuilder =>
+//            {
+//                webBuilder.UseStartup<Startup>();
+//            });
 
-        }
-}
+//        }
+//}
